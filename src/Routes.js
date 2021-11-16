e2a35b8bebd09a1275ecf31bbb1293d0e3a301e8
설혜린 <seolhyelin@seolhyelin-ui-MacBook-Pro-2.local>
2021-11-16 21:56:01 +0900

    Nav, 카카오톡 로그인 구현 완료

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Nav from '../src/components/Nav/Nav';
+import Nav from './components/Nav/Nav';
 import Main from './pages/Main/Main';
-import DetailPage from './pages/DetailPage/DetailPage';
-import Cart from './pages/Cart/Cart';
 
 class Routes extends React.Component {
   render() {
@@ -12,8 +10,6 @@ class Routes extends React.Component {
         <Nav />
         <Switch>
           <Route exact path="/" component={Main} />
-          <Route exact path="/DetailPage" component={DetailPage} />
-          <Route exact path="/Cart" component={Cart} />
         </Switch>
       </Router>
     );
