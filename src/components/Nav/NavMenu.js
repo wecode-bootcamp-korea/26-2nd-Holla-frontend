a74742fc99a877e19384e9d241b629acc20d9f0d
설혜린 <seolhyelin@seolhyelin-ui-MacBook-Pro-2.local>
2021-11-18 08:09:18 +0900

    Nav, Nav menu 레이아웃 완료

--- src/components/Nav/NavMenu.js
+++ src/components/Nav/NavMenu.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
+import { RiKakaoTalkFill } from 'react-icons/ri';
 import './NavMenu.scss';
 
 export class NavMenu extends Component {
   render() {
     return (
       <aside className="menuList">
-        <div className="exitButton">x</div>
+        <input type="button" className="exitButton" value="X" />
         <div className="loginContainer">
-          <input type="button" className="loginButton" value="카카오 로그인" />
-          <img alt="kakao" src="kakao.png" />
+          <input
+            type="button"
+            className="loginButton"
+            value="카카오톡 로그인"
+          />
+          <RiKakaoTalkFill className="kakaoIcon" />
         </div>
 
-        <div className="imgContainer" />
+        <div className="imgContainer">
+          <img src="https://images.unsplash.com/photo-1571258126466-de842f523326?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjF8fGJvb2t8ZW58MHwxfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
+          <div className="imgCenter">
+            <div className="saying">"</div>
+            <div className="saying">Whenever you read a good book, </div>
+            <div className="saying"> somewhere in the world a door opens </div>
+            <div className="saying">to allow in more light.</div>
+            <div className="saying">"</div>
+            <div className="saying">-Vera Nazzarian-</div>
+          </div>
+        </div>
       </aside>
     );
   }
